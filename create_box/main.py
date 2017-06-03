37d19330249e150943bf5d929eca218829b06fb6
Jon Handy <jonhandy@gmail.com>
2017-06-03 15:58:22 +0000

    first commit

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,43 @@
 
 
 def create_box(height, width, character):
-    pass
-
+    string = ""
+    if height >= 1 and width >= 1:
+        for h in range(height):
+            for w in range(width):
+                string += character
+            string += "\n"
+            
+        return string
+    else:
+        return 'invalid size'
+        
+def empty_box(height, width, character):
+    string = ""
+    if height >= 3 and width >= 3:
+        for h in range(height):
+            if h == 0 or h == height - 1:
+                for j in range(width):
+                    string += character
+                string += "\n"
+            else:
+                
+                for w in range(width):
+                    if w == 0 or w == width - 1:
+                        string += character
+                    else:
+                        string += " "
+                string += "\n"
+            
+        return string
+    else:
+        return 'invalid size'
+    
+        
+    
+        
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -22,8 +22,28 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
         self.assertEqual(create_box(3, 4, '*'), first_box_expected)
+        
+    def test_third_box(self):
+        self.assertEqual(create_box(3, 24, 'x'), third_box_expected)
 
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
+        
+    def test_invalid_size(self):
+        self.assertEqual(create_box(0, 1, '@'), 'invalid size')
+        
+    def test_invalid_size_two(self):
+        self.assertEqual(create_box(1, 0, '@'), 'invalid size')
 
     # Add your own test using third_box_expected
+from create_box import empty_box
+
+first_empty_box_expected = """
+***
+* *
+***
+""".lstrip()
+
+class TestCreateEmptyBox(unittest.TestCase):
+    def test_empty_box(self):
+        self.assertEqual(empty_box(3, 3, '*'), first_empty_box_expected)
